fcfee670ab363a92257a345764aad93087215565
wanchong <wilson3cy@163.com>
2019-11-08 16:58:29 +0800

    添加ts配置文件

--- greeter.ts
+++ greeter.ts
@@ -31,20 +31,20 @@
 
 // TypeScript支持JavaScript的新特性，比如支持基于类的面向对象编程。
 class Student {
-    fullName: string;
-    // 在构造函数的参数上使用public等同于创建了同名的成员变量
-    constructor(public firstName, public middleInitial, public lastName) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+	fullName: string;
+	// 在构造函数的参数上使用public等同于创建了同名的成员变量
+	constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+		this.fullName = firstName + " " + middleInitial + " " + lastName;
+	}
 }
 
 interface Person {
-    firstName: string;
-    lastName: string;
+	firstName: string;
+	lastName: string;
 }
 
 function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+	return "Hello, " + person.firstName + " " + person.lastName;
 }
 
 let user = new Student("Jane", "M.", "User");
